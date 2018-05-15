8a8dce9cf34e29cd99b36d61ec51b48288236b2a
Didier Gbenou <dyg23@drexel.edu>
2018-05-15 15:28:11 -0400

    Fixed Firebase error in env.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyArnIn9jkEe-I-o7z7G7-rWFAatbyWsCqE',
+    authDomain: 'mypersonalwebsite-6f3fd.firebaseapp.com',
+    databaseURL: 'https://mypersonalwebsite-6f3fd.firebaseio.com',
+    projectId: 'mypersonalwebsite-6f3fd',
+    storageBucket: 'mypersonalwebsite-6f3fd.appspot.com',
+    messagingSenderId: '1035576370812'
+  }
 };
